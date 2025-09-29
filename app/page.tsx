c4e9c5953fd0124dcb26934ba28b9dbefa6d98cb
Raul Glez Rdguez <raulglezrdguez69@gmail.com>
2025-09-29 11:15:25 -0400

    chadcn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <p>Hello</p>
+      <Button variant={"primary"}>Hello</Button>
+      <Button className="backdrop-blur-lg bg-white/10 text-white border-white/20 hover:bg-white/20 hover:transform hover:scale-105">
+        Hello
+      </Button>
     </div>
   );
 }
